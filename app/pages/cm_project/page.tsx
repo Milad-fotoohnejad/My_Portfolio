fb197a1a48796f5a066f51cd3446b3e1f0abb9a9
Milad Fotoohnejad <milad.fotoohnejad@itas.ca>
2023-10-05 23:25:43 -0700

    Updated the layout for the classic mafia project

--- app/pages/cm_project/page.tsx
+++ app/pages/cm_project/page.tsx
@@ -1,11 +1,38 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import { FaJava, FaReact, FaNodeJs, FaDatabase, FaGithub } from "react-icons/fa";
+import Head from "next/head";
+import {
+  FaJava,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaGithub,
+  FaJs,
+  FaGitlab,
+  FaCss3,
+} from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function CmProject() {
   const [currentImg, setCurrentImg] = useState(0);
-  const imgList = ['/cm1.png', '/cm2.png', '/cm3.png', '/cm4.png', '/cm5.png', '/cm6.png', '/cm7.png', '/cm8.png', '/cm9.png', '/cm10.png', '/cm11.png'];
+  const imgList = [
+    "/cm1.png",
+    "/cm2.png",
+    "/cm3.png",
+    "/cm4.png",
+    "/cm5.png",
+    "/cm6.png",
+    "/cm7.png",
+    "/cm8.png",
+    "/cm9.png",
+    "/cm10.png",
+    "/cm11.png",
+  ];
 
   const nextSlide = () => {
     setCurrentImg(currentImg === imgList.length - 1 ? 0 : currentImg + 1);
@@ -14,54 +41,113 @@ export default function CmProject() {
   const prevSlide = () => {
     setCurrentImg(currentImg === 0 ? imgList.length - 1 : currentImg - 1);
   };
-
   return (
-    <div className="op-back rounded-md p-5">
-      <div className="text-white">
-        <h1 className="font-mono text-4xl mb-6">Classic Mafia Game</h1>
+    <>
+      <div className="op-back rounded-md p-5">
+        <div className="text-white">
+          <h1 className="font-mono text-4xl mb-6">Classic Mafia Game</h1>
 
-        <section>
-          <h2 className="font-mono text-2xl mb-4">Introduction</h2>
-          <p className="mb-6">Description about the project goes here.</p>
-          <p className="mb-6">Associated with Vancouver Island University. I have developed a web application for the popular social deduction game, Classic Mafia for my capstone project. This real-time multiplayer game provides an engaging gameplay experience, offering players the opportunity to interact in real-time to reveal the hidden Mafia members among them.</p>
-          <p className="mb-6">My tech stack for this project comprises Firebase, ExpressJS, ReactJS, and NodeJS, a potent combination for creating a real-time, highly interactive game. I&apos;m utilizing JavaScript (ReactJS) as the primary programming language to ensure optimal performance and wide compatibility.</p>
-        </section>
-
-        <section>
-          <h2 className="font-mono text-2xl mb-4">Gallery</h2>
-          <div className="relative h-60 md:h-96">
-            <button className="absolute z-10 left-0 mt-28 md:mt-44 p-2 rounded-full bg-gray-600 text-white" onClick={prevSlide}>
-              {'<'}
-            </button>
-            {imgList.map((img, index) => (
-              <div key={index} className={index === currentImg ? 'opacity-100 duration-700 transition-opacity' : 'opacity-0 duration-700 transition-opacity'}>
-                <Image src={img} alt="Game image" layout="fill" objectFit="contain" />
+          <section className="mb-5">
+            <div className="relative h-96 md:h-128">
+              <button
+                className="absolute z-10 left-0 mt-48 md:mt-64 p-2 rounded-full bg-gray-600 text-white"
+                onClick={prevSlide}
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+              {imgList.map((img, index) => (
+                <div
+                  key={index}
+                  className={
+                    index === currentImg
+                      ? "opacity-100 duration-700 transition-opacity"
+                      : "opacity-0 duration-700 transition-opacity"
+                  }
+                >
+                  <Image
+                    src={img}
+                    alt="Game image"
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+              ))}
+              <button
+                className="absolute z-10 right-0 mt-48 md:mt-64 p-2 rounded-full bg-gray-600 text-white"
+                onClick={nextSlide}
+              >
+                <FontAwesomeIcon icon={faChevronRight} />
+              </button>
+              <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-2 mt-4">
+                {imgList.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`w-3 h-3 mb-3 rounded-full ${
+                      index === currentImg ? "bg-green-400" : "bg-gray-400"
+                    }`}
+                  ></div>
+                ))}
               </div>
-            ))}
-            <button className="absolute z-10 right-0 mt-28 md:mt-44 p-2 rounded-full bg-gray-600 text-white" onClick={nextSlide}>
-              {'>'}
-            </button>
-          </div>
-        </section>
+            </div>
+          </section>
 
-        <section>
-          <h2 className="font-mono text-2xl mb-4">Tech Stack</h2>
-          <div className="flex space-x-4">
-            <FaJava className="text-3xl" />
-            <FaReact className="text-3xl" />
-            <FaNodeJs className="text-3xl" />
-            <FaDatabase className="text-3xl" />
-          </div>
-        </section>
-
-        <section>
-          <h2 className="font-mono text-2xl mb-4">GitHub Repository</h2>
-          <a href="https://github.com/Milad-fotoohnejad/ClassicMafia" className="text-blue-500" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="text-3xl" />
-            <span className="ml-2">View on GitHub</span>
-          </a>
-        </section>
+          <section className="op-back rounded-md p-3">
+            <h2 className="font-mono text-2xl mb-4">Introduction</h2>
+            <p className="mb-6">
+              In the Mafia game website, Lord, who is the narrator of the
+              instructions of the game, is going to be played online. The
+              purpose of the website is to let people to skip the role of the
+              Lord in the game, and everybody can play. The website will act as
+              the Lord role, give the instruction, distribute the roles randomly
+              to people, and take their decisions on the game flow.
+            </p>
+            <p className="mb-6">
+              Associated with Vancouver Island University. I have developed a
+              web application for the popular social deduction game, Classic
+              Mafia for my capstone project. This real-time multiplayer game
+              provides an engaging gameplay experience, offering players the
+              opportunity to interact in real-time to reveal the hidden Mafia
+              members among them.
+            </p>
+            <p className="mb-6">
+              <h2 className="font-mono text-2xl mb-4">Tech Stack</h2>
+              Technologies used for this project includes React.js for this
+              website and typically using JavaScript for both the front-end and
+              back-end development. On the front-end, React.js would be used to
+              build the user interface and handle the user interactions. For the
+              front-end UI design I used HTML5, CSS3 and Tailwind. On the
+              back-end, Node.js, which is a JavaScript runtime built on Chrome's
+              V8 JavaScript engine, could be used as the back-end framework. For
+              the database, Firebase, which is a NoSQL database could be used to
+              store the game information and user information. To handle the
+              communication between the front-end and back-end, a library like
+              Axios could be used to make HTTP requests to the back-end API.
+              Additionally, libraries like express could be used to handle the
+              state management and routing respectively. This stack is commonly
+              referred to as FERN stack (FireBase, Express.js, React.js,
+              Node.js).
+            </p>
+            <section>
+              <div className="flex justify-center text-green-400 space-x-4">
+                <FaJs className="text-5xl" />
+                <FaReact className="text-5xl" />
+                <FaNodeJs className="text-5xl" />
+                <a
+                  href="https://github.com/Milad-fotoohnejad/ClassicMafia"
+                  className="text-white animate-blink"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="text-5xl" />
+                </a>
+                <FaDatabase className="text-5xl" />
+                <FaCss3 className="text-5xl" />
+                <FaGitlab className="text-5xl" />
+              </div>
+            </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/pages/viu_project/page.tsx
+++ app/pages/viu_project/page.tsx
@@ -1,34 +1,134 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { FaJava, FaReact, FaNodeJs, FaDatabase } from 'react-icons/fa';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Head from "next/head";
+import {
+  FaJava,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaGithub,
+} from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
+export default function VbProject() {
+  const [currentImg, setCurrentImg] = useState(0);
+  const imgList = [
+    "/bv-1.png",
+    "/bv-2.png",
+    "/bv-3.png",
+    "/bv-4.png",
+    "/bv-5.png",
+    "/bv-6.png",
+    "/bv-7.png",
+  ];
 
-export default function CmProject() {
+  const nextSlide = () => {
+    setCurrentImg(currentImg === imgList.length - 1 ? 0 : currentImg + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentImg(currentImg === 0 ? imgList.length - 1 : currentImg - 1);
+  };
   return (
-    <div className='content'>
-      <h1 className='font-mono text-4xl mb-6'>Classic Mafia Game</h1>
+    <>
+      <div className="op-back rounded-md p-5">
+        <div className="text-white">
+          <h1 className="font-mono text-4xl mb-6">Classic Mafia Game</h1>
 
-      <section>
-        <h2 className='font-mono text-2xl mb-4'>Introduction</h2>
-        <p className='mb-6'>
-          Description about the project goes here.
-        </p>
-      </section>
+          <section className="op-back rounded-md p-3">
+            <h2 className="font-mono text-2xl mb-4">Introduction</h2>
+            <p className="mb-6">Description about the project goes here.</p>
+            <p className="mb-6">
+              Associated with Vancouver Island University. I have developed a
+              web application for the popular social deduction game, Classic
+              Mafia for my capstone project. This real-time multiplayer game
+              provides an engaging gameplay experience, offering players the
+              opportunity to interact in real-time to reveal the hidden Mafia
+              members among them.
+            </p>
+            <p className="mb-6">
+              My tech stack for this project comprises Firebase, ExpressJS,
+              ReactJS, and NodeJS, a potent combination for creating a
+              real-time, highly interactive game. I&apos;m utilizing JavaScript
+              (ReactJS) as the primary programming language to ensure optimal
+              performance and wide compatibility.
+            </p>
+          </section>
 
-      <section>
+          <section>
+            <h2 className="font-mono text-2xl mb-4">Project Pictures</h2>
+            <div className="relative h-96 md:h-128">
+              <button
+                className="absolute z-10 left-0 mt-48 md:mt-64 p-2 rounded-full bg-gray-600 text-white"
+                onClick={prevSlide}
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+              {imgList.map((img, index) => (
+                <div
+                  key={index}
+                  className={
+                    index === currentImg
+                      ? "opacity-100 duration-700 transition-opacity"
+                      : "opacity-0 duration-700 transition-opacity"
+                  }
+                >
+                  <Image
+                    src={img}
+                    alt="Game image"
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+              ))}
+              <button
+                className="absolute z-10 right-0 mt-48 md:mt-64 p-2 rounded-full bg-gray-600 text-white"
+                onClick={nextSlide}
+              >
+                <FontAwesomeIcon icon={faChevronRight} />
+              </button>
+              <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-2 mt-4">
+                {imgList.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`w-3 h-3 mb-3 rounded-full ${
+                      index === currentImg ? "bg-green-400" : "bg-gray-400"
+                    }`}
+                  ></div>
+                ))}
+              </div>
+            </div>
+          </section>
 
-      </section>
+          <section>
+            <h2 className="font-mono text-2xl mb-4">Tech Stack</h2>
+            <div className="flex space-x-4">
+              <FaJava className="text-3xl" />
+              <FaReact className="text-3xl" />
+              <FaNodeJs className="text-3xl" />
+              <FaDatabase className="text-3xl" />
+            </div>
+          </section>
 
-      <section>
-        <h2 className='font-mono text-2xl mb-4'>Tech Stack</h2>
-        <div className='flex space-x-4'>
-          <FaJava className='text-3xl' />
-          <FaReact className='text-3xl' />
-          <FaNodeJs className='text-3xl' />
-          <FaDatabase className='text-3xl' />
-          {/* Add more icons as needed */}
+          <section>
+            <h2 className="font-mono text-2xl mb-4">GitHub Repository</h2>
+            <a
+              href="https://github.com/Milad-fotoohnejad/ClassicMafia"
+              className="text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="text-3xl" />
+              <span className="ml-2">View on GitHub</span>
+            </a>
+          </section>
         </div>
-      </section>
-    </div>
+      </div>
+    </>
   );
 }
